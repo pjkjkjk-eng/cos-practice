a00cb13797fd3acd6aa216daed8a1956f0b52fa2
User <xfine016son@gmail.com>
2026-03-31 14:29:31 +0000

    added all

---
+++ 4/problem.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int check_validity(int a, int b) {}
+int sum_up(int a, int b) {}
+int main(int argc, char *argv[])
+{
+    return 0;
+}
--- 5/pointer-2.cpp
+++ 5/pointer-2.cpp
@@ -6,8 +6,19 @@ int main(int argc, char *argv[]){
 	int *p;
 	int a, i;
 
-	for(i=0, i<BUFLEN; i++)
+	for(i=0, i<BUFLEN; i++;)
 		arr[i] = 10-i;
 
 		a=5;
 		p = &a;
+
+	cout << "a = " << a << endl;
+	cout << "p = " << p << endl;
+
+	*p = 10;
+
+	cout << "a = " << a << endl;
+	cout << "p = " << p << endl;
+
+	return 0;
+}
